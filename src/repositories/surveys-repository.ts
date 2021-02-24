0ab4548da9129207f72781eb0d00049fb0a9e3f6
Felipe C. Araujo <felipe.s-04@hotmail.com>
2021-02-24 10:36:14 -0300

    add surveys-repository

---
+++ src/repositories/surveys-repository.ts
@@ -0,0 +1,10 @@
+import { EntityRepository, Repository } from "typeorm";
+import { Survey } from "../models/survey";
+
+
+@EntityRepository(Survey)
+class SurveysRepository extends Repository<Survey>{
+    //
+}
+
+export { SurveysRepository };
