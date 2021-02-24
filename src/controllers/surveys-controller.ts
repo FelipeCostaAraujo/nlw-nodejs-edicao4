14db8114e0b2203d4b97e570c16006051b40935e
Felipe C. Araujo <felipe.s-04@hotmail.com>
2021-02-24 10:34:17 -0300

    add surveys-controller

---
+++ src/controllers/surveys-controller.ts
@@ -0,0 +1,10 @@
+import { Request, Response } from "express";
+
+class SurveysController {
+    async create(request: Request, response: Response) {
+        //
+    }
+}
+
+
+export { SurveysController }
