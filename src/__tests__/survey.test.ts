11d23869e4400caa6ba773b58876b899c1a00058
Felipe C. Araujo <felipe.s-04@hotmail.com>
2021-02-24 12:40:08 -0300

    add surveys test

---
+++ src/__tests__/survey.test.ts
@@ -0,0 +1,34 @@
+import request from 'supertest';
+import { app } from '../app';
+
+import createConnection from '../database';
+
+describe("Surveys", () => {
+
+    beforeAll(async () => {
+        const connection = await createConnection();
+        await connection.runMigrations();
+    });
+
+    it("Should be able to create a new survey", async () => {
+        const response = await request(app).post("/api/surveys").send({
+            title: "any_title",
+            description: "any_description"
+        });
+
+        expect(response.status).toBe(201);
+        expect(response.body).toHaveProperty("id");
+    });
+
+    it("Should be able to get all surveys", async () => {
+        await request(app).post("/api/surveys").send({
+            title: "any_title",
+            description: "any_description"
+        });
+
+        const response = await request(app).get("/api/surveys");
+
+        expect(response.body.length).toBe(2);
+    });
+
+});
