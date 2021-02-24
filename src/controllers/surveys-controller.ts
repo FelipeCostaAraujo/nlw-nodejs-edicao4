1e65b59ef1296841135df9b4ebc570e3023827b7
Felipe C. Araujo <felipe.s-04@hotmail.com>
2021-02-24 10:41:20 -0300

    implements surveys create method

--- src/controllers/surveys-controller.ts
+++ src/controllers/surveys-controller.ts
@@ -1,8 +1,21 @@
 import { Request, Response } from "express";
+import { getCustomRepository } from "typeorm";
+import { SurveysRepository } from "../repositories/surveys-repository";
 
 class SurveysController {
     async create(request: Request, response: Response) {
-        //
+        const { title, description } = request.body;
+
+        const surveysRepository = getCustomRepository(SurveysRepository);
+
+        const survey = surveysRepository.create({
+            title,
+            description
+        });
+
+        await surveysRepository.save(survey);
+
+        return response.status(201).json(survey);
     }
 }
 
--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
+import { SurveysController } from './controllers/surveys-controller';
 import UserController from './controllers/user-controller';
 
 const router = Router();
 
 const userController = new UserController();
+const surveysController = new SurveysController();
 
 router.post('/users', userController.create);
+router.post('/surveys', surveysController.create)
 
 export { router }
