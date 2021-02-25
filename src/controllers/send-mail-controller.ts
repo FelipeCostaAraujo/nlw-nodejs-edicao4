29db4ec5d6dbc4d676d8ea25e1f6a09ccf7a8c8c
Felipe C. Araujo <felipe.s-04@hotmail.com>
2021-02-25 11:29:16 -0300

    create sendmailcontroller

---
+++ src/controllers/send-mail-controller.ts
@@ -0,0 +1,9 @@
+import { Request, Response } from "express";
+
+class SendMailController {
+    async execute(request: Request, response: Response) {
+        //
+    }
+}
+
+export { SendMailController }
