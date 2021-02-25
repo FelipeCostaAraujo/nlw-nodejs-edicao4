542208f7002c855ad2e69bb12be23b224f2dab56
Felipe C. Araujo <felipe.s-04@hotmail.com>
2021-02-25 12:20:11 -0300

    sending mail

--- src/controllers/send-mail-controller.ts
+++ src/controllers/send-mail-controller.ts
@@ -3,6 +3,7 @@ import { getCustomRepository } from "typeorm";
 import { SurveysRepository } from "../repositories/surveys-repository";
 import { UsersRepository } from "../repositories/users-repository";
 import { SurveysUsersRepository } from "../repositories/surveys-user-repository";
+import SendmailService from "../services/sendmail-service";
 
 class SendMailController {
     async execute(request: Request, response: Response) {
@@ -20,8 +21,8 @@ class SendMailController {
             });
         }
 
-        const surveysAlreadyExists = await surveysRepository.findOne({ id: survey_id });
-        if (!surveysAlreadyExists) {
+        const survey = await surveysRepository.findOne({ id: survey_id });
+        if (!survey) {
             return response.status(400).json({
                 error: "Survey does not exists!"
             });
@@ -29,10 +30,12 @@ class SendMailController {
 
         const surveyUser = surveysUsersRepository.create({
             user_id: userAlredyExists.id,
-            survey_id: surveysAlreadyExists.id
+            survey_id: survey.id
         });
 
         await surveysUsersRepository.save(surveyUser);
+
+        await SendmailService.execute(email, survey.title, survey.description)
 
         return response.status(201).json(surveyUser);
     }
--- src/services/sendmail-service.ts
+++ src/services/sendmail-service.ts
@@ -1,14 +1,40 @@
-import nodemailer from 'nodemailer';
+import nodemailer, { Transporter } from 'nodemailer';
 
 class SendMailService {
 
+    private client: Transporter;
+
     constructor() {
-        //
+        nodemailer.createTestAccount().then((account => {
+            const transporter = nodemailer.createTransport({
+                host: account.smtp.host,
+                port: account.smtp.port,
+                secure: account.smtp.secure,
+                auth: {
+                    user: account.user,
+                    pass: account.pass,
+                },
+            });
+
+            this.client = transporter;
+
+        })).catch((err) => {
+            console.log(err);
+        });
     }
 
-    async execute() {
-        //
+    async execute(to: string, subject: string, body: string) {
+        const message = await this.client.sendMail({
+            to,
+            subject,
+            html: body,
+            from: "NSP <noreplay@nps.com.br>"
+        });
+
+        console.log('Message sent: %s', message.messageId);
+        // Preview only available when sending through an Ethereal account
+        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(message));
     }
 }
 
-export { SendMailService }
+export default new SendMailService();
