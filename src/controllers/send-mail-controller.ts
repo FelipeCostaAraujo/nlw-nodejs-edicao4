733a8a7b34b5d2e190ca85c26e93472ba870bf04
Felipe C. Araujo <felipe.s-04@hotmail.com>
2021-02-25 11:34:40 -0300

    verify user, survey existis

--- src/controllers/send-mail-controller.ts
+++ src/controllers/send-mail-controller.ts
@@ -1,7 +1,31 @@
 import { Request, Response } from "express";
+import { getCustomRepository } from "typeorm";
+import { SurveysRepository } from "../repositories/surveys-repository";
+import { UsersRepository } from "../repositories/users-repository";
 
 class SendMailController {
     async execute(request: Request, response: Response) {
+        const { email, survey_id } = request.body;
+
+        const usersRepository = getCustomRepository(UsersRepository);
+        const surveysRepository = getCustomRepository(SurveysRepository);
+        const surveysUsersRepository = getCustomRepository(SurveysRepository);
+
+        const userAlredyExists = await usersRepository.findOne({ email });
+
+        if (!userAlredyExists) {
+            return response.status(400).json({
+                error: "User does not exists!"
+            });
+        }
+
+        const surveysAlreadyExists = await surveysRepository.findOne({ id: survey_id });
+        if (!surveysAlreadyExists) {
+            return response.status(400).json({
+                error: "Survey does not exists!"
+            });
+        }
+
         //
     }
 }
