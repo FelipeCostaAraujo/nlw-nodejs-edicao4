48e4e912b4d69bcaccaf2576ce8bcc3f45cc4399
Felipe C. Araujo <felipe.s-04@hotmail.com>
2021-02-25 11:42:08 -0300

    create route send

--- src/router.ts
+++ src/router.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { SendMailController } from './controllers/send-mail-controller';
 import { SurveysController } from './controllers/surveys-controller';
 import UserController from './controllers/user-controller';
 
@@ -6,11 +7,13 @@ const router = Router();
 
 const userController = new UserController();
 const surveysController = new SurveysController();
-
+const sendMainController = new SendMailController();
 
 router.get('/surveys', surveysController.show)
 
 router.post('/users', userController.create);
 router.post('/surveys', surveysController.create)
+
+router.post('/sendMain', sendMainController.execute)
 
 export { router }
