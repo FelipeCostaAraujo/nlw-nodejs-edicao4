f30932627883fced34d95523b0653c17b2d91f52
Felipe C. Araujo <felipe.s-04@hotmail.com>
2021-02-26 16:56:36 -0300

    add middleware app.ts

--- app/app.ts
+++ app/app.ts
@@ -1,8 +1,9 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import bodyParser from 'body-parser';
 import { router } from '../src/router';
 import 'reflect-metadata';
 import createConnection from '../src/database';
+import { AppError } from '../src/errors/app-error';
 
 createConnection();
 const app = express();
@@ -12,6 +13,18 @@ app.use(bodyParser.json({
 }));
 
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use((err: Error, request: Request, response: Response, _next: NextFunction) => {
+    if (err instanceof AppError) {
+        return response.status(err.statusCode).json({
+            message: err.message
+        });
+    }
+    return response.status(500).json({
+        status: "Error",
+        message: `Internal server error ${err.message}`
+    });
+});
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
