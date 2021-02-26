5a482ec99da315eaa035e04a89f1371635a2e0fe
Felipe C. Araujo <felipe.s-04@hotmail.com>
2021-02-26 16:51:29 -0300

    add cors

--- app/app.ts
+++ app/app.ts
@@ -13,6 +13,13 @@ app.use(bodyParser.json({
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-access-token');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+});
+
 app.use(express.json());
 app.use("/api", router);
 
