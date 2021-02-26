bb38348f5ec6cfd898ca6e9be6cb69607c69c9a1
Felipe C. Araujo <felipe.s-04@hotmail.com>
2021-02-26 16:32:58 -0300

    add yup validate

--- src/controllers/user-controller.ts
+++ src/controllers/user-controller.ts
@@ -1,11 +1,23 @@
 import { Request, Response } from "express";
 import { getCustomRepository } from "typeorm";
 import { UsersRepository } from "../repositories/users-repository";
+import * as yup from 'yup';
 
 class UserController {
 
     async create(request: Request, response: Response) {
         const { email, name } = request.body;
+
+        const schema = yup.object().shape({
+            name: yup.string().required(),
+            email: yup.string().email().required()
+        });
+
+        try {
+            await schema.validate(request.body, { abortEarly: false });
+        } catch (ex) {
+            return response.status(400).json({ error: ex });
+        }
 
         const usersRepository = getCustomRepository(UsersRepository);
 
