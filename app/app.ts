ecc9cc44901b72faa9f633238beb8e2c6c013a14
Felipe C. Araujo <felipe.s-04@hotmail.com>
2021-02-26 16:50:52 -0300

    add bodyparser

--- app/app.ts
+++ app/app.ts
@@ -1,10 +1,17 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 import { router } from '../src/router';
 import 'reflect-metadata';
 import createConnection from '../src/database';
 
 createConnection();
 const app = express();
+
+app.use(bodyParser.json({
+    limit: '10mb'
+}));
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.json());
 app.use("/api", router);
