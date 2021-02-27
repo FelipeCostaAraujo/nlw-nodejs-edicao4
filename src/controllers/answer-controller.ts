43154206199c1995b4f38912c5b6fb36a4b37fc0
Felipe C. Araujo <felipe.s-04@hotmail.com>
2021-02-26 23:13:08 -0300

    protect router surveys

--- src/controllers/answer-controller.ts
+++ src/controllers/answer-controller.ts
@@ -2,7 +2,6 @@ import { Request, Response } from "express";
 import { getCustomRepository } from "typeorm";
 import { AppError } from "../errors/app-error";
 import { SurveysUsersRepository } from "../repositories/surveys-user-repository";
-import { getFullUrl } from "../utils/get-full-url";
 
 class AnswerController {
     async execute(request: Request, response: Response) {
@@ -10,11 +9,9 @@ class AnswerController {
         const { value } = request.params;
         const { u } = request.query;
 
-        const getUrl = getFullUrl(request);
+        const surveysUsersRepository = getCustomRepository(SurveysUsersRepository);
 
-        const serveysUsersREpository = getCustomRepository(SurveysUsersRepository);
-
-        const surveyUser = await serveysUsersREpository.findOne({
+        const surveyUser = await surveysUsersRepository.findOne({
             id: String(u)
         });
 
@@ -24,7 +21,7 @@ class AnswerController {
 
         surveyUser.value = Number(value);
 
-        await serveysUsersREpository.save(surveyUser);
+        await surveysUsersRepository.save(surveyUser);
 
         return response.status(201).json(surveyUser);
     }
--- src/router.ts
+++ src/router.ts
@@ -5,8 +5,10 @@ import { NpsController } from './controllers/nps-controller';
 import { SendMailController } from './controllers/send-mail-controller';
 import { SurveysController } from './controllers/surveys-controller';
 import { UserController } from './controllers/user-controller';
+import AuthService from './services/auth-service';
 
 const router = Router();
+const authService = new AuthService()
 
 const userController = new UserController();
 const surveysController = new SurveysController();
@@ -17,13 +19,12 @@ const authController = new AuthController();
 
 router.get('/surveys', surveysController.show);
 router.get('/answers/:value', answerController.execute);
+router.get('/nps/:survey_id', npsController.execute);
 
 router.post('/users', userController.create);
-router.post('/surveys', surveysController.create);
+router.post('/surveys', authService.authorize, surveysController.create);
 router.post('/auth', authController.auth);
-
 router.post('/sendMain', sendMainController.execute);
 
-router.get('/nps/:survey_id', npsController.execute);
 
 export { router };
