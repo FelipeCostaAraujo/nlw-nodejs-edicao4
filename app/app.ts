8ec61dbb8d9852398a6abfdacc3471ba3d5cc518
Felipe C. Araujo <felipe.s-04@hotmail.com>
2021-07-22 16:59:55 -0300

    remove cors

--- app/app.ts
+++ app/app.ts
@@ -22,6 +22,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/api", router);
 
+
 app.use(
     (err: Error, request: Request, response: Response, _next: NextFunction) => {
         if (err instanceof AppError) {
